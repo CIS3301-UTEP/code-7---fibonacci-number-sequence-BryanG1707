3ef1abc0a8215a1ada9f057a5ddbcbfe8a32aeec
BryanG1707 <bguerrero10@miners.utep.edu>
2024-11-03 00:55:42 -0600

    SUBMISSSION 2?

--- code_12.py
+++ code_12.py
@@ -11,11 +11,11 @@ def get_fibonacci_number_sequence(number):
     if number == 0:
         return [0]
     if number == 1:
-        return [1,1]
+        return [0,1]
     
     previous_x = 0
     previous_y = 1
-    number_sequence = [0,1]
+    number_sequence = [1]
 
     for i in range(2, number + 1):
         current = previous_x + previous_y
